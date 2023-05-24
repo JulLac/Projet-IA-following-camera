d0ea17a8cd12f25d1033e9e769e594f8bee844b7
Julian-bt <julian.burtin@isen-ouest.yncrea.fr>
2023-05-24 15:43:26 +0200

    update

--- code/app/main.py
+++ code/app/main.py
@@ -3,9 +3,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 
 # tell interpreter where to look
 sys.path.insert(0,"..")
+
 from gui.interfaceQT import InterfaceQT
-
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
