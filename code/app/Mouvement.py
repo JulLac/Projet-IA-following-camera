979fed38a540685f005d3f7fada391efa1c3f84d
Julian-bt <julian.burtin@isen-ouest.yncrea.fr>
2023-06-01 16:16:00 +0200

    Mouvement.py 01/06/2023 ajout commentaire

--- code/app/Mouvement.py
+++ code/app/Mouvement.py
@@ -14,8 +14,13 @@ Gauche="Gauche"
 Droite="Droite"
 Centre="Centre"
 
-#x=horizontal et y=vertical
-
+"""
+sources librairie: https://github.com/pimoroni/pantilt-hat 
+                   http://docs.pimoroni.com/pantilthat/
+x=horizontal et y=vertical
+Haut   <-- (-90°)  0 (90°) --> Bas
+Gauche <-- (-90°)  0 (90°) --> Droite
+"""
 class Mouvement_camera:
     def __init__(self,hauteur,largeur,xmax,xmin,ymax,ymin):
         self.hauteur=hauteur
@@ -35,21 +40,25 @@ class Mouvement_camera:
         self.max_degre_y_bas=90
 
     def bouger_camera(self):
-        self.calculate_centre_tete()
+        """
+        But: Bouger la caméra hozizontalement et verticalement pour centrer la bounding box reçu au centre de l'objectif
+        :return: Mouvement de la caméra
+        """
         position_horizontal=self.get_position_horizontal()
         position_vertical=self.get_position_vertical()
+        self.calculate_centre_tete()
         self.enable_servo()#mettre en route les moteurs
 
-        #Obtenir un pas fort ou faible en fct de la distance de la tête en fonction de la distance de la personne
+        #Obtenir un pas fort ou faible en fct de la distance de la bounding box
         pas_horizontal,pas_vertical=self.gestion_pas_camera_Horizontal_Vertical()
 
         #Horizontal
-        if(self.centre_tete[0] > self.center[0]+threshold_horizontal):#Tête à droite
+        if(self.centre_tete[0] > self.center[0]+threshold_horizontal):
             if(position_horizontal-pas_horizontal>=self.max_degre_x_gauche):
                 self.mouvement_horizontal(position_horizontal-pas_horizontal)#Gauche
             else:
                 self.mouvement_horizontal(self.max_degre_x_gauche)
-        elif(self.centre_tete[0] < self.center[0]-threshold_horizontal):#Tête à gauche
+        elif(self.centre_tete[0] < self.center[0]-threshold_horizontal):
             if(position_horizontal+pas_horizontal<=self.max_degre_x_droite):
                 self.mouvement_horizontal(position_horizontal+pas_horizontal)#Droite
             else:
@@ -68,6 +77,12 @@ class Mouvement_camera:
                 self.mouvement_vertical(self.max_degre_y_haut)
 
     def gestion_pas_camera_Horizontal_Vertical(self):
+        """
+        But: Déterminer un pas fort ou faible en fct de la distance du centre de la bounding box et du centre de l'objectif avec selon un certain seuil
+        :return: liste_pas type(liste)
+                 liste_pas[0] = pas fort/faible horizontal
+                 liste_pas[1] = pas fort/faible vertical
+        """
         liste_pas=[]
         #Horizontal
         if((self.centre_tete[0] - self.center[0]+threshold_horizontal >threshold_pas) or (self.center[0]-threshold_horizontal -self.centre_tete[0] > threshold_pas)):
@@ -83,6 +98,10 @@ class Mouvement_camera:
         return liste_pas[0],liste_pas[1]
 
     def balayage(self):
+        """
+        But: Balayer le champ de vision pour détecter une personne (Centre->Gauche->Droite->Gauche->Droite->Centre->Pause 5 secondes->..etc)
+        :return: Mouvement de la caméra
+        """
         position_horizontal=self.get_position_horizontal()
         position_vertical=self.get_position_vertical()
         if(self.direction==Centre):
@@ -107,14 +126,14 @@ class Mouvement_camera:
                     if(position_horizontal-pas_balayage>self.max_degre_x_gauche):
                         self.mouvement_horizontal(position_horizontal-pas_balayage)#Gauche
                     else:
-                        self.mouvement_horizontal(self.max_degre_x_gauche)
+                        self.mouvement_horizontal(self.max_degre_x_gauche)#Limite Gauche
                         self.direction=Droite
                         self.boucle_balayage+=1
                 else:
                     if(position_horizontal+pas_balayage<self.max_degre_x_droite):
                         self.mouvement_horizontal(position_horizontal+pas_balayage)#Droite
                     else:
-                        self.mouvement_horizontal(self.max_degre_x_droite)
+                        self.mouvement_horizontal(self.max_degre_x_droite)#Limite Droite
                         self.direction=Gauche
             else:
                 if(position_horizontal!=0):#centrer de manière smooth à la fin du balayage
@@ -128,34 +147,56 @@ class Mouvement_camera:
                         self.enable_servo()#mettre en route les moteurs
 
     def centrer(self):
+        """
+        But : Centrer la caméra en sa position initiale choisie
+        :return: Mouvement de la caméra
+        """
         self.mouvement_horizontal(position_degre_defaut_x)
         self.mouvement_vertical(position_degre_defaut_y)
     
     def centrer_bridage(self,x_gauche,x_droite,y_haut,y_bas):
+        """
+        But : Centrer la caméra en sa position initiale en fonction du bridage en degré choisi
+        :return: Mouvement de la caméra
+        """
         self.mouvement_horizontal((x_droite+x_gauche)/2)
         self.mouvement_vertical((y_bas+y_haut)/2)
 
     def reset(self):
+        """
+        But : Réinitialiser les variables permettant le balayage
+        """
         self.direction=Centre
         self.temps=0
         self.boucle_balayage=0
 
     def calculate_centre_tete(self):
-        #calcul_quart_moitie=(self.ymax+self.ymin/2)/4
-        #self.centre_tete=[(self.xmax+self.xmin)/2,((self.ymax+self.ymin)/2)-calcul_quart_moitie] #entre 0 et 1
+        """
+        But : Centrer la caméra en sa position initiale en fonction du bridage en degré choisi
+        :return: self.centre_tete[0] -> coordonnées horizontal
+                 self.centre_tete[1] -> coordonnées vertical
+        """
         self.centre_tete=[(self.xmax+self.xmin)/2,(self.ymax+self.ymin)/2] #entre 0 et 1
 
     def mouvement_horizontal(self,degre):
+        """
+        But : Bouger la caméra horizontalement
+        :return: Mouvement de la caméra
+        """
         if(degre<=self.max_degre_x_droite and degre>=self.max_degre_x_gauche):
-            pantilthat.pan(degre)
+            pantilthat.pan(degre)#servo 1
         else:
             print("limite atteinte 'degré' pour mouvement horizontal, degré=",degre," ;max degré=[",self.max_degre_x_droite,',',self.max_degre_x_gauche,']')
 
     def mouvement_vertical(self,degre):
+        """
+        But : Bouger la caméra verticalement
+        :return: Mouvement de la caméra
+        """
         if(degre<=self.max_degre_y_bas and degre>=self.max_degre_y_haut):
-            pantilthat.tilt(degre)
+            pantilthat.tilt(degre)#servo 2
         else:
-            print("limite atteinte 'degré' pour mouvement vertical hors limite, degré=",degre," ;max degré=[",self.max_degre_y_haut,',',self.max_degre_y_bas,']')
+            print("limite atteinte 'degré' pour mouvement vertical, degré=",degre," ;max degré=[",self.max_degre_y_haut,',',self.max_degre_y_bas,']')
 
     def get_position_horizontal(self):
         return pantilthat.get_pan()#en degré servo 1
@@ -176,12 +217,12 @@ class Mouvement_camera:
         self.ymin=round(ymin,2)
 
     def disable_servo(self):
-        pantilthat.servo_enable(1,False)
-        pantilthat.servo_enable(2,False)
+        pantilthat.servo_enable(1,False)#servo 1
+        pantilthat.servo_enable(2,False)#servo 2
 
     def enable_servo(self):
-        pantilthat.servo_enable(1,True)
-        pantilthat.servo_enable(2,True)
+        pantilthat.servo_enable(1,True)#servo 1
+        pantilthat.servo_enable(2,True)#servo 2
 
     def set_max_degre_x_gauche(self,degre):
         self.max_degre_x_gauche=degre
